fc2ff9d7977379b6036265966dd80dee6ab9356c
JaroslawKAA <kaszczak.jaroslaw@outlook.com>
2024-08-11 13:25:11 +0200

    Implement pooling and spawning agents

--- Assets/Scripts/Agents/AgentManager.cs
+++ Assets/Scripts/Agents/AgentManager.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class AgentManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // SERIALIZED
+    [SerializeField] Agent agentPrefab;
+
+    // PRIVATE
+    LinkedPool<Agent> agentPool;
+    
+
+    // UNITY EVENTS
+    void Awake()
     {
-        
+        agentPool = new LinkedPool<Agent>(createFunc: Pool_Spawn);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDestroy()
     {
-        
+        agentPool.Clear();
+        agentPool = null;
+    }
+
+    // METHODS
+    Agent Spawn() => agentPool.Get();
+    
+    Agent Pool_Spawn()
+    {
+        Agent agentInstance = Instantiate(agentPrefab);
+        return agentInstance;
     }
 }
